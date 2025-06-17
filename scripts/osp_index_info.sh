1bae3e60158325c26266a590f9833174b9f89e0d
aThorp96 <athorp@redhat.com>
2025-06-17 15:58:55 -0400

    improve output of index parser

--- scripts/osp_index_info.sh
+++ scripts/osp_index_info.sh
@@ -6,9 +6,49 @@
 
 set -euo pipefail
 
+declare -A repositories
+repositories=(
+	["pipelines-cache-rhel9"]="openshift-pipelines/tekton-caches"
+	["pipelines-chains-controller-rhel9"]="tektoncd/chains"
+	["pipelines-cli-tkn-rhel9"]="tektoncd/cli"
+	["pipelines-console-plugin-rhel9"]="openshift-pipelines/console-plugin"
+	["pipelines-controller-rhel9"]="tektoncd/pipeline"
+	["pipelines-entrypoint-rhel9"]="tektoncd/pipeline"
+	["pipelines-events-rhel9"]="tektoncd/pipeline"
+	["pipelines-git-init-rhel9"]="openshift-pipelines/ecosystem-images"
+	["pipelines-hub-api-rhel9"]="tektoncd/hub"
+	["pipelines-hub-db-migration-rhel9"]="tektoncd/hub"
+	["pipelines-hub-ui-rhel9"]="tektoncd/hub"
+	["pipelines-manual-approval-gate-controller-rhel9"]="openshift-pipelines/manual-approval-gate"
+	["pipelines-manual-approval-gate-webhook-rhel9"]="openshift-pipelines/manual-approval-gate"
+	["pipelines-nop-rhel9"]="tektoncd/pipeline"
+	["pipelines-opc-rhel9"]=""
+	["pipelines-operator-bundle"]="tektoncd/operator"
+	["pipelines-operator-proxy-rhel9"]="tektoncd/operator"
+	["pipelines-operator-webhook-rhel9"]="tektoncd/operator"
+	["pipelines-pipelines-as-code-cli-rhel9"]="openshift-pipelines/pipelines-as-code"
+	["pipelines-pipelines-as-code-controller-rhel9"]="openshift-pipelines/pipelines-as-code"
+	["pipelines-pipelines-as-code-watcher-rhel9"]="openshift-pipelines/pipelines-as-code"
+	["pipelines-pipelines-as-code-webhook-rhel9"]="openshift-pipelines/pipelines-as-code"
+	["pipelines-pruner-controller-rhel9"]="openshift-pipelines/tektoncd-pruner"
+	["pipelines-resolvers-rhel9"]="tektoncd/pipeline"
+	["pipelines-results-api-rhel9"]="tektoncd/results"
+	["pipelines-results-retention-policy-agent-rhel9"]="tektoncd/results"
+	["pipelines-results-watcher-rhel9"]="tektoncd/results"
+	["pipelines-rhel9-operator"]="openshift-pipelines/operator"
+	["pipelines-sidecarlogresults-rhel9"]="tektoncd/pipeline"
+	["pipelines-triggers-controller-rhel9"]="tektoncd/triggers"
+	["pipelines-triggers-core-interceptors-rhel9"]="tektoncd/triggers"
+	["pipelines-triggers-eventlistenersink-rhel9"]="tektoncd/triggers"
+	["pipelines-triggers-webhook-rhel9"]="tektoncd/triggers"
+	["pipelines-webhook-rhel9"]="tektoncd/pipeline"
+	["pipelines-workingdirinit-rhel9"]="tektoncd/pipeline"
+)
+
 function parse_component_image() {
     IMAGE="${1:?No component image provided}"
 	IMAGE_REF=$(echo "${IMAGE}" | rev | cut -d '/' -f 1 | rev)
+	IMAGE_REPO=$(echo "${IMAGE_REF}" | cut -d '@' -f 1)
 	DOWNSTREAM_COMMIT=""
 	UPSTREAM_COMMIT=""
 
@@ -20,6 +60,10 @@ function parse_component_image() {
     	[[ "${DOWNSTREAM_COMMIT}" == "" ]] || OUT="${OUT}\n    downstream_commit: ${DOWNSTREAM_COMMIT}"
 
     	podman cp "${CONTAINER}:/kodata/HEAD" "${CONTAINER}_head" 2>/dev/null && UPSTREAM_COMMIT=$(cat "${CONTAINER}_head") || echo -n ""
+		if [[ -n "${UPSTREAM_COMMIT}" && -n "${repositories[${IMAGE_REPO}]}" ]]; then
+    		REPO=${repositories["${IMAGE_REPO}"]}
+    		UPSTREAM_COMMIT="https://github.com/${REPO}/commit/${UPSTREAM_COMMIT}"
+		fi
     	[[ "${UPSTREAM_COMMIT}" == "" ]] || OUT="${OUT}\n    upstream_commit: ${UPSTREAM_COMMIT}"
 	fi
 
